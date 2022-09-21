ba605554e23796d85083c7c809ab349ca541bf53
amakhanNisum <amakhan@nisum.com>
2022-09-21 17:04:34 -0500

    changed Oauth to SSO instead

--- omsfrontend/src/app/app.module.ts
+++ omsfrontend/src/app/app.module.ts
@@ -5,22 +5,40 @@ import { OAuthModule } from 'angular-oauth2-oidc';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { GoogleApiComponent } from './login/login.component';
+import { LoginPageComponent } from './login/login.component';
 import { NavbarComponent } from './navbar/navbar.component';
+
+import { SocialLoginModule, SocialAuthServiceConfig } from 'angularx-social-login';
+import {  GoogleLoginProvider } from 'angularx-social-login';
 
 @NgModule({
   declarations: [
     AppComponent,
-    GoogleApiComponent,
+    LoginPageComponent,
     NavbarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    SocialLoginModule,
     OAuthModule.forRoot() 
   ],
-  providers: [],
+  providers: [    {
+    provide: 'SocialAuthServiceConfig',
+    useValue: {
+      autoLogin: true,
+      providers: [
+        {
+          id: GoogleLoginProvider.PROVIDER_ID,
+          provider: new GoogleLoginProvider(
+            '624796833023-clhjgupm0pu6vgga7k5i5bsfp6qp6egh.apps.googleusercontent.com'
+          ),
+        }
+      ],
+    } as SocialAuthServiceConfig,
+  },
+],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- omsfrontend/src/app/login/login.component.ts
+++ omsfrontend/src/app/login/login.component.ts
@@ -1,34 +1,71 @@
 import { Component, OnInit } from '@angular/core';
-import { GoogleApiService, UserInformation } from '../google-api.service';
+import { SocialAuthService } from "angularx-social-login";
+import { GoogleLoginProvider } from "angularx-social-login";
+import { SocialUser } from "angularx-social-login";
 
 @Component({
-  selector: 'app-google-api',
+  selector: 'app-login-page',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class GoogleApiComponent implements OnInit {
+export class LoginPageComponent implements OnInit {
 
+  user: SocialUser | undefined;
+  GoogleLoginProvider = GoogleLoginProvider;
 
-  ngOnInit(): void {
-  }
-  userInfo?: UserInformation
+  constructor(private readonly _authService: SocialAuthService) {}
 
-  constructor(private readonly googleApi: GoogleApiService) {
-    googleApi.userProfileSubject.subscribe( info => {
-      this.userInfo = info
-    })
-  }
-
-  isLoggedIn(): boolean {
-    return this.googleApi.isLoggedIn()
+  ngOnInit() {
+    this._authService.authState.subscribe((user) => {
+      this.user = user;
+    });
   }
 
-  logout() {
-    this.googleApi.signOut()
+  signInWithGL(): void {
+    this._authService.signIn(GoogleLoginProvider.PROVIDER_ID);
   }
 
-  login(){
-    this.googleApi.login()
+  signOut(): void {
+    this._authService.signOut();
   }
 
+  refreshGoogleToken(): void {
+    this._authService.refreshAuthToken(GoogleLoginProvider.PROVIDER_ID);
+  }
+  
 }
+
+// import { Component, OnInit } from '@angular/core';
+// import { GoogleApiService, UserInformation } from '../google-api.service';
+
+// @Component({
+//   selector: 'app-google-api',
+//   templateUrl: './login.component.html',
+//   styleUrls: ['./login.component.css']
+// })
+// export class GoogleApiComponent implements OnInit {
+
+
+//   ngOnInit(): void {
+//   }
+//   userInfo?: UserInformation
+
+//   constructor(private readonly googleApi: GoogleApiService) {
+//     googleApi.userProfileSubject.subscribe( info => {
+//       this.userInfo = info
+//     })
+//   }
+
+//   isLoggedIn(): boolean {
+//     return this.googleApi.isLoggedIn()
+//   }
+
+//   logout() {
+//     this.googleApi.signOut()
+//   }
+
+//   login(){
+//     this.googleApi.login()
+//   }
+
+// }
